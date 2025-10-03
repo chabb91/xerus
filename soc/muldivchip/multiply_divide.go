88eaa3b632573812093d125597d5a1be0f39dcb4
chabb91 <>
2025-10-03 20:42:13 +0200

    created the chip that is responsible for the Multiplication and Division registers

--- soc/muldivchip/multiply_divide.go
+++ soc/muldivchip/multiply_divide.go
@@ -1,2 +1,66 @@
 package muldivchip
 
+const divCycleCount = 96
+const mulCycleCount = 48
+
+type MulDiv struct {
+	Wrmpya, wrmpyb         byte // multiplicands a and b
+	Wrdivl, Wrdivh, wrdivb byte // dividend low dividend high divisor
+	Rddivl, Rddivh         byte // quotient of divide low and high
+	Rdmpyl, Rdmpyh         byte // divide remainder or multiplication product
+
+	divDelay int
+	mulDelay int
+}
+
+func NewMulDiv() *MulDiv {
+	return &MulDiv{
+		Wrdivl: 0xFF,
+		Wrdivh: 0xFF,
+		Wrmpya: 0xFF}
+}
+
+func (md *MulDiv) SetMultiplicandB(mulB byte) {
+	md.wrmpyb = mulB
+	md.mulDelay = mulCycleCount
+}
+
+func (md *MulDiv) SetDivisorB(divB byte) {
+	md.wrdivb = divB
+	md.divDelay = divCycleCount
+}
+
+func (md *MulDiv) StepCycle() {
+	if md.divDelay > 0 {
+		md.divDelay--
+		if md.divDelay == 0 {
+			if md.wrdivb != 0 {
+				divA := createWord(md.Wrdivh, md.Wrdivl)
+				md.Rddivh, md.Rddivl = splitWord(divA / uint16(md.wrdivb))
+				md.Rdmpyh, md.Rdmpyl = splitWord(divA % uint16(md.wrdivb))
+			} else {
+				md.Rddivh = 0xFF
+				md.Rddivl = 0xFF
+				md.Rdmpyh = md.Wrdivh
+				md.Rdmpyl = md.Wrdivl
+			}
+		}
+	}
+	if md.mulDelay > 0 {
+		md.mulDelay--
+		if md.mulDelay == 0 {
+			md.Rdmpyh, md.Rdmpyl = splitWord(uint16(md.Wrmpya * md.wrmpyb))
+		}
+	}
+}
+
+func createWord(high, low byte) uint16 {
+	return (uint16(high) << 8) | uint16(low)
+}
+
+func splitWord(word uint16) (high, low byte) {
+	high = byte(word >> 8)
+	low = byte(word & 0xFF)
+
+	return high, low
+}
