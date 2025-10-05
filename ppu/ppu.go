b9c2a575897ff99395bd9168e5ce3e6902277888
chabb91 <>
2025-10-05 19:51:23 +0200

    started working on the registers that load the data into and from the PPU VRAM

---
+++ ppu/ppu.go
@@ -0,0 +1,60 @@
+package ppu
+
+import "fmt"
+
+type PPU struct {
+	VRAM  []uint16 //video RAM
+	CGRAM []byte   //Color/Paletter RAM
+	OAM   []byte   //object attribute memory/sprites
+
+	vmadd  uint16
+	vmdata uint16
+	vmain  *VMAIN
+
+	FBlank, VBlank, HBlank bool
+}
+
+func NewPPU() *PPU {
+	return &PPU{
+		vmain: newVMAIN(),
+		VRAM:  make([]uint16, 0x8000),
+		CGRAM: make([]byte, 0x200),
+		OAM:   make([]byte, 0x220)}
+}
+
+// Some of these registers can only be read and written to at specific times defined by the blanking periods
+// TODO
+func (ppu *PPU) Read(addr uint16) (byte, error) {
+	switch addr {
+	default:
+		return 0, fmt.Errorf("invalid PPU register read at $%04X", addr)
+	}
+}
+
+func (ppu *PPU) Write(addr uint16, value byte) error {
+	switch addr {
+	case 0x2115:
+		ppu.vmain.Setup(value)
+	case 0x2116:
+		ppu.vmadd = (ppu.vmadd & 0xFF00) | uint16(value)
+	case 0x2117:
+		ppu.vmadd = (ppu.vmadd & 0xFF) | (uint16(value) << 8)
+	case 0x2118:
+		remapped_addr := ppu.vmain.remap(ppu.vmadd) & 0x7FFF
+		ppu.VRAM[remapped_addr] = (ppu.VRAM[remapped_addr] & 0xFF00) | uint16(value)
+
+		if !ppu.vmain.incrementOnHighByte {
+			ppu.vmadd = (ppu.vmadd + ppu.vmain.incrementAmount) & 0x7FFF
+		}
+	case 0x2119:
+		remapped_addr := ppu.vmain.remap(ppu.vmadd) & 0x7FFF
+		ppu.VRAM[remapped_addr] = (ppu.VRAM[remapped_addr] & 0x00FF) | (uint16(value) << 8)
+
+		if ppu.vmain.incrementOnHighByte {
+			ppu.vmadd = (ppu.vmadd + ppu.vmain.incrementAmount) & 0x7FFF
+		}
+	default:
+		return fmt.Errorf("invalid PPU register write at $%04X", addr)
+	}
+	return nil
+}
