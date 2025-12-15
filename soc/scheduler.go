e1b4ace41f9dc76021c1367a7c6166b88bdd7632
chabb91 <>
2025-12-15 01:25:17 +0100

    rewrote the scheduler to be half decent (again)

--- soc/scheduler.go
+++ soc/scheduler.go
@@ -1,8 +1,13 @@
 package soc
 
-func (soc SoC) Run() {
+const PPU_TICK_RATE = uint64(2)
+const DMA_OVERHEAD = uint64(4)
+const CPU_BASE_TICK_RATE = uint64(3)
+const SPU_TICK_RATE = uint64(12)
+const CPU_REFRESH_DURATION = uint64(20)
 
-	var cnt uint64
+func (soc SoC) Run() {
+	var cnt uint64 //the dma/cpu cycle counter is counting down due to the variable access speed
 	var cnt1 uint64
 	var cnt2 uint64
 	var cyclesSinceReset uint64
@@ -10,26 +15,17 @@ func (soc SoC) Run() {
 
 	var prevDmaActive bool
 
-	cpuTickRate := 3
-	cpuRefreshDuration := 20
-	//dmaTickRate := 4
-	ppuTickRate := 2
-	spuTickRate := 12
-
-	cnt1 = uint64(spuTickRate) - 1
-	cnt2 = uint64(ppuTickRate) - 1
-
 	for {
 		cnt1++
 		cnt2++
 		cyclesSinceReset++
 
 		soc.MulDiv.StepCycle()
-		if cnt1 == uint64(spuTickRate) {
+		if cnt1 == SPU_TICK_RATE {
 			soc.Spu.StepCycle()
 			cnt1 = 0
 		}
-		if cnt2 == uint64(ppuTickRate) {
+		if cnt2 == PPU_TICK_RATE {
 			soc.Ppu.Step()
 			cnt2 = 0
 		}
@@ -39,40 +35,34 @@ func (soc SoC) Run() {
 			//refresh pause begins at 538 cycles into the first scanline of the first frame,
 			//and thereafter some multiple of 8 cycles after the previous pause that comes closest to 536
 			soc.Ppu.Refresh = false
-			cnt += uint64(cpuRefreshDuration) - 1
+			cnt += CPU_REFRESH_DURATION
 		}
 
-		if cnt > 0 {
+		if cnt > 1 {
 			cnt--
 			continue
 		}
 
-		// TODO if i keep decrementing like this in the future the cycle counts should be adjusted instead
-		// so theres no unnecessary subtraction
 		dmaActive := soc.Dma.IsInProgress()
 		dmaHandoff := dmaActive && !prevDmaActive
 		if !dmaActive || dmaHandoff {
 			soc.Cpu.StepCycle()
+			cnt = CPU_BASE_TICK_RATE //TODO introduce variable cycle count
 
 			if dmaHandoff {
-				cyclesSincePause = cyclesSinceReset
-				alignment := (4 - (cyclesSinceReset & 3)) // TODO this should be sinceReset+nextCycleCnt
-				cnt = 4 + alignment - 1
-			} else {
-				cnt = uint64(cpuTickRate) - 1
+				cyclesSincePause = cyclesSinceReset + cnt
+				alignment := (4 - ((cyclesSincePause) & 3)) // TODO this should be sinceReset+nextCycleCnt
+				cnt += DMA_OVERHEAD + alignment
 			}
-
 			prevDmaActive = dmaActive
 		} else {
-			cycles := soc.Dma.Step()
+			cnt = soc.Dma.Step()
+
 			dmaStillActive := soc.Dma.IsInProgress()
 			if !dmaStillActive {
-				alignment := (4 - (((cyclesSinceReset + cycles) - cyclesSincePause) & 3)) & 3
-				cnt = cycles + alignment - 1
-			} else {
-				cnt = cycles - 1
+				alignment := (4 - (((cyclesSinceReset + cnt) - cyclesSincePause) & 3)) & 3
+				cnt += alignment
 			}
-
 			prevDmaActive = dmaStillActive
 		}
 	}
