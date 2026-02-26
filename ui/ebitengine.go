a89e6e1ee71e366b6ed447bf2cc97f3107f354c9
chabb91 <>
2026-02-26 02:26:01 +0100

    cleaned the code up a bit. this is fun but not meaningfully faster :(

--- ui/ebitengine.go
+++ ui/ebitengine.go
@@ -7,11 +7,9 @@ import (
 )
 
 const BufferHeight = 478
-const BufferWidth = 256
 const BufferWidthShift = 8
 
-const MaxScreenHeight = BufferHeight
-const MaxScreenWidth = BufferWidth * 2
+const ScreenWidth = 1 << (BufferWidthShift + 1)
 
 const shaderSource = `
 //kage:unit pixels
@@ -38,7 +36,7 @@ func init() {
 	var err error
 	bgrShader, err = ebiten.NewShader([]byte(shaderSource))
 	if err != nil {
-		panic("Shader Bad")
+		panic("Kage: Shader compilation failed.")
 	}
 }
 
@@ -47,21 +45,12 @@ type UiConfig interface {
 	GetInputMapping() []SnesInput
 }
 
-type SnesColorData struct {
-	Color1, Color2 uint16
-	Brightness     byte
-}
-
-func (scd *SnesColorData) SetColor(color1, color2 uint16, brightness byte) {
-	scd.Color1, scd.Color2, scd.Brightness = color1, color2, brightness
-}
-
 type Framebuffer struct {
-	swap            chan *[BufferHeight << (BufferWidthShift + 3)]byte
-	f, B            *[BufferHeight << (BufferWidthShift + 3)]byte //H*512*4
+	swap chan *[BufferHeight << (BufferWidthShift + 3)]byte
+	f, B *[BufferHeight << (BufferWidthShift + 3)]byte //H*512*4
+
 	backPointer     unsafe.Pointer
 	backPointerBase unsafe.Pointer
-	idx             uintptr
 	lineCnt         int
 
 	CurrentHeight int
@@ -82,32 +71,13 @@ func NewFramebuffer() *Framebuffer {
 }
 
 func (fb *Framebuffer) WriteDot(color1, color2 uint16, brightness byte) {
-	/*
-		fb.B[fb.idx] = byte(color1)
-		fb.B[fb.idx+1] = byte(color1 >> 8)
-		fb.B[fb.idx+2] = brightness
-		fb.B[fb.idx+3] = 0
-
-		fb.B[fb.idx+4] = byte(color2)
-		fb.B[fb.idx+5] = byte(color2 >> 8)
-		fb.B[fb.idx+6] = brightness
-		fb.B[fb.idx+7] = 0
-		fb.idx += 8
-		if fb.Interlace == 1 && fb.idx&0x7FF == 0 {
-			fb.lineCnt++
-			fb.idx += 0x800
-			if fb.idx >= len(fb.B) || fb.lineCnt == fb.CurrentHeight {
-				fb.idx = 0x800
-			}
-		}
-	*/
 	*(*uint64)(fb.backPointer) = (uint64(color1) | uint64(brightness)<<16 |
 		uint64(color2)<<32 | uint64(brightness)<<48)
 	step := 8 + uintptr(fb.backPointer)
 	if fb.Interlace == 1 && step&0x7FF == 0 {
 		fb.lineCnt++
 		step += 0x800
-		if /*fb.idx >= len(fb.B) ||*/ fb.lineCnt == fb.CurrentHeight {
+		if fb.lineCnt == fb.CurrentHeight {
 			step = uintptr(fb.backPointerBase) + 0x800
 		}
 	}
@@ -115,7 +85,6 @@ func (fb *Framebuffer) WriteDot(color1, color2 uint16, brightness byte) {
 }
 
 func (fb *Framebuffer) Swap() {
-	fb.idx = 0
 	fb.lineCnt = 0
 	fb.backPointer = unsafe.Pointer(&fb.B[0])
 	fb.backPointerBase = unsafe.Pointer(&fb.B[0])
@@ -148,10 +117,10 @@ func NewEmulatorDisplay(fb *Framebuffer, config UiConfig) *EmulatorDisplay {
 	controllers := config.GetInputMapping()
 	return &EmulatorDisplay{
 		fb:                fb,
-		ActiveImage:       updateActiveImage(MaxScreenHeight, displayScale),
-		transformedBuffer: make([]byte, 4*MaxScreenWidth*MaxScreenHeight),
-		ScreenWidth:       MaxScreenWidth,
-		ScreenHeight:      MaxScreenHeight,
+		ActiveImage:       updateActiveImage(BufferHeight, displayScale),
+		transformedBuffer: make([]byte, 4*ScreenWidth*BufferHeight),
+		ScreenWidth:       ScreenWidth,
+		ScreenHeight:      BufferHeight,
 		ScalingFactor:     displayScale,
 
 		Controller0: controllers[0],
@@ -162,8 +131,8 @@ func NewEmulatorDisplay(fb *Framebuffer, config UiConfig) *EmulatorDisplay {
 }
 
 func updateActiveImage(height int, scalingFactor float64) *ebiten.Image {
-	activeImage := ebiten.NewImage(MaxScreenWidth, height)
-	ebiten.SetWindowSize(int(float64(MaxScreenWidth)*scalingFactor), int(float64(height)*scalingFactor))
+	activeImage := ebiten.NewImage(ScreenWidth, height)
+	ebiten.SetWindowSize(int(float64(ScreenWidth)*scalingFactor), int(float64(height)*scalingFactor))
 
 	return activeImage
 }
@@ -178,8 +147,7 @@ func (ed *EmulatorDisplay) Update() error {
 			ed.ActiveImage = updateActiveImage(newHeight, ed.ScalingFactor)
 		}
 
-		//ed.transformedBuffer = frame[:]
-		ed.ActiveImage.WritePixels(frame[:ed.ScreenWidth*ed.ScreenHeight*4])
+		ed.ActiveImage.WritePixels(frame[:(ed.ScreenWidth*ed.ScreenHeight)<<2])
 	default:
 		// no new frame yet
 	}
@@ -195,51 +163,13 @@ func (ed *EmulatorDisplay) Draw(screen *ebiten.Image) {
 	if ed.ActiveImage == nil {
 		return
 	}
-	/*
-		op := &ebiten.DrawImageOptions{}
-		scaleX := float64(1)
-		scaleY := float64(int(2 >> ed.fb.Interlace))
-		op.GeoM.Scale(scaleX, scaleY)
-		screen.DrawImage(ed.ActiveImage, op)
-	*/
 	op := &ebiten.DrawRectShaderOptions{}
 	op.Images[0] = ed.ActiveImage
-	scaleX := float64(1)
 	scaleY := float64(int(2 >> ed.fb.Interlace))
-	op.GeoM.Scale(scaleX, scaleY)
+	op.GeoM.Scale(1.0, scaleY)
 	screen.DrawRectShader(ed.ScreenWidth, ed.ScreenHeight, bgrShader, op)
 }
 
 func (ed *EmulatorDisplay) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return ed.ScreenWidth, ed.ScreenHeight
-}
-
-func (ed *EmulatorDisplay) convertBGR15ToRGBA(buffer *[BufferHeight][BufferWidth]SnesColorData) {
-	shift := ed.fb.Interlace ^ 1
-	for y := 0; y < ed.ScreenHeight>>shift; y++ {
-		for x := 0; x < BufferWidth; x++ {
-			v := buffer[y][x]
-			i := (y<<BufferWidthShift + x) << 3
-
-			r := float32(v.Color1 & 0x1F << 3)
-			g := float32((v.Color1 >> 5) & 0x1F << 3)
-			b := float32((v.Color1 >> 10) & 0x1F << 3)
-
-			scale := float32(v.Brightness) / 15
-
-			ed.transformedBuffer[i+0] = byte(r * scale)
-			ed.transformedBuffer[i+1] = byte(g * scale)
-			ed.transformedBuffer[i+2] = byte(b * scale)
-			ed.transformedBuffer[i+3] = 0xFF // alpha always fully opaque
-
-			r = float32(v.Color2 & 0x1F << 3)
-			g = float32((v.Color2 >> 5) & 0x1F << 3)
-			b = float32((v.Color2 >> 10) & 0x1F << 3)
-
-			ed.transformedBuffer[i+4] = byte(r * scale)
-			ed.transformedBuffer[i+5] = byte(g * scale)
-			ed.transformedBuffer[i+6] = byte(b * scale)
-			ed.transformedBuffer[i+7] = 0xFF
-		}
-	}
 }
