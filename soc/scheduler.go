148f22d913be27e6444fd90638a27fdcbb821753
chabb91 <>
2026-05-11 14:59:55 +0200

    rewrote the scheduler to a fast forward model. this is more flexible, faster, and it doesnt care about cycle numbers really because there arent any wasted cycles anymore. ran some tests, seems like there are no regressions but no compatibility improvements either. the hdma bug still haunts me just the same

--- soc/scheduler.go
+++ soc/scheduler.go
@@ -6,27 +6,25 @@ import (
 	"github.com/chabb91/xerus/internal/constants"
 )
 
-const DMA_OVERHEAD = constants.CYCLE_8
-const CPU_REFRESH_DURATION = constants.CYCLE_40
 const CPU_DELAY_INTERRUPT_AFTER_DMA = uint64(2) //cpu cycle count
 
 func (soc SoC) Run() {
 	var cpuCnt uint64 //the dma/cpu cycle counter is counting down due to the variable access speed
 	var ppuCnt uint64
 	var copCnt uint64
-	var cyclesSinceReset uint64
-	var cyclesSincePause uint64
+	var prevApuStep uint64
+	var cyclesAtPause uint64
 	var nmiDelay uint64
 
-	var prevDmaActive bool
+	var prevDmaActive bool = false
 	var nmiSignalBeforeDma bool
 	var irqSignalBeforeDma bool
 	var nmiTriggeredDuringDma bool
 	var irqTriggeredDuringDma bool
 
-	var apuDebt int64
-	PrecisionScale := int64(1_000_000_000)
-	apuRatio := int64((float64(constants.SPU_BASE_FREQUENCY) /
+	var apuDebt uint64
+	PrecisionScale := uint64(1_000_000_000)
+	apuRatio := uint64((float64(constants.SPU_BASE_FREQUENCY) /
 		float64(soc.timing.baseFrequency)) *
 		float64(PrecisionScale))
 
@@ -34,53 +32,41 @@ func (soc SoC) Run() {
 	cyclesPerPeriod := soc.timing.cyclesPerInterval
 	soc.timing.start()
 
-	for {
-		cyclesSinceReset++
-		cyclesSinceLastInterval++
-		if cyclesSinceLastInterval == cyclesPerPeriod {
-			cyclesSinceLastInterval = 0
-			soc.timing.sync()
-		}
-
-		if ppuCnt > 1 {
-			ppuCnt--
-		} else {
-			ppuCnt = soc.Ppu.Step()
-		}
-
-		apuDebt += apuRatio
-		if apuDebt >= PrecisionScale { //only works because ratio << scale so 1 step is the max
-			apuDebt -= PrecisionScale
-			soc.Spu.Step()
-		}
-
-		if soc.Cop != nil {
-			if copCnt > 1 {
-				copCnt--
-			} else {
-				copCnt = soc.Cop.Step()
-			}
-		}
-
+	stepPpu := func() {
+		ppuCnt += soc.Ppu.Step()
 		if soc.Ppu.Refresh {
 			//TODO there is some variation to this:
 			//refresh pause begins at 538 cycles into the first scanline of the first frame,
 			//and thereafter some multiple of 8 cycles after the previous pause that comes closest to 536
 			soc.Ppu.Refresh = false
-			cpuCnt += CPU_REFRESH_DURATION
+			cpuCnt += constants.CYCLE_40
 		}
+	}
+	var stepCop func()
+	var getNextStep func() uint64
+	if soc.Cop == nil {
+		stepCop = func() { return }
+		getNextStep = func() uint64 { return min(ppuCnt, cpuCnt) }
+	} else {
+		stepCop = func() { copCnt += soc.Cop.Step() }
+		getNextStep = func() uint64 { return min(ppuCnt, copCnt, cpuCnt) }
+	}
 
-		if cpuCnt > 1 {
-			cpuCnt--
-			continue
+	syncApu := func(nextStep uint64) {
+		apuDebt += apuRatio * (nextStep - prevApuStep)
+		for apuDebt >= PrecisionScale {
+			apuDebt -= PrecisionScale
+			soc.Spu.Step()
 		}
+		prevApuStep = nextStep
+	}
 
-		dmaActive := soc.Dma.IsInProgress()
-		dmaHandoff := dmaActive && !prevDmaActive
-		if !dmaActive || dmaHandoff {
+	stepCpu := func() {
+		if !prevDmaActive {
+			prevDmaActive = soc.Dma.IsInProgress()
 			soc.MulDiv.StepCycle()
 			soc.Cpu.StepCycle()
-			cpuCnt = soc.Cpu.CyclesTaken
+			cpuCnt += soc.Cpu.CyclesTaken
 
 			if nmiTriggeredDuringDma || irqTriggeredDuringDma {
 				if nmiDelay > 1 {
@@ -93,35 +79,57 @@ func (soc SoC) Run() {
 				}
 			}
 
-			if dmaHandoff {
-				cyclesSincePause = cyclesSinceReset + cpuCnt
-				alignment := (4 - ((cyclesSincePause) & 3)) // TODO this should be sinceReset+nextCycleCnt
-				cpuCnt += DMA_OVERHEAD + alignment
+			if prevDmaActive { //handoff
+				cyclesAtPause = cpuCnt
+				// TODO this should be sinceReset+nextCycleCnt
+				cpuCnt += (constants.CYCLE_8 - ((cyclesAtPause) &
+					(constants.CYCLE_8 - 1)))
+				cpuCnt += constants.CYCLE_8 //transfer overhead
 				nmiSignalBeforeDma = soc.Cpu.NmiSignal
 				irqSignalBeforeDma = soc.Cpu.IrqSignal
 			}
-			prevDmaActive = dmaActive
 		} else {
-			if soc.Dma.Mdmaen != 0 {
-				if !nmiSignalBeforeDma && soc.Cpu.NmiSignal {
-					nmiDelay = CPU_DELAY_INTERRUPT_AFTER_DMA
-					soc.Cpu.NmiSignal = false
-					nmiTriggeredDuringDma = true
-				}
-				if !irqSignalBeforeDma && soc.Cpu.IrqSignal {
-					nmiDelay = CPU_DELAY_INTERRUPT_AFTER_DMA
-					soc.Cpu.IrqSignal = false
-					irqTriggeredDuringDma = true
-				}
+			//if soc.Dma.Mdmaen != 0 {
+			if !nmiSignalBeforeDma && soc.Cpu.NmiSignal {
+				nmiDelay = CPU_DELAY_INTERRUPT_AFTER_DMA
+				soc.Cpu.NmiSignal = false
+				nmiTriggeredDuringDma = true
 			}
+			if !irqSignalBeforeDma && soc.Cpu.IrqSignal {
+				nmiDelay = CPU_DELAY_INTERRUPT_AFTER_DMA
+				soc.Cpu.IrqSignal = false
+				irqTriggeredDuringDma = true
+			}
+			//}
 
-			cpuCnt = soc.Dma.Step()
+			cpuCnt += soc.Dma.Step()
 
 			prevDmaActive = soc.Dma.IsInProgress()
 			if !prevDmaActive {
-				alignment := (4 - (((cyclesSinceReset + cpuCnt) - cyclesSincePause) & 3))
-				cpuCnt += alignment
+				cpuCnt += (constants.CYCLE_8 - (((cpuCnt) - cyclesAtPause) &
+					(constants.CYCLE_8 - 1)))
 			}
+		}
+	}
+
+	for {
+		nextStep := getNextStep()
+		if nextStep-cyclesSinceLastInterval >= cyclesPerPeriod {
+			cyclesSinceLastInterval = nextStep
+			soc.timing.sync()
+		}
+
+		if ppuCnt == nextStep {
+			stepPpu()
+		}
+
+		if copCnt == nextStep {
+			stepCop()
+		}
+
+		if cpuCnt == nextStep {
+			syncApu(nextStep)
+			stepCpu()
 		}
 	}
 }
